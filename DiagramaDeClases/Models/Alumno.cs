225f1c2e01e9a0998ae8223e7f0c40622c1324d5
triboga <triboga@gmail.com>
2017-06-01 09:29:52 +0200

    Cambios de orden

---
+++ DiagramaDeClases/Models/Alumno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace DiagramaDeClases
+{
+    public class Alumno:Persona, evaluable
+    {
+        private Double nota_;
+
+        public Alumno(double nota, string nombre, string edad):base(nombre, edad)
+        {
+            nota_ = nota;
+        }
+
+        public double Nota
+
+        {
+            get
+            {
+                return nota_;
+            }
+            set
+            {
+                nota_ = value;
+            }
+        }
+
+        public override void evalua(double nota)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
