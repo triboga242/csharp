ab05e8061b2e3376ed655dfb31d3b030ad97c1e0
triboga <triboga@gmail.com>
2017-06-01 10:04:51 +0200

    tumtumpa

---
+++ DiagramaDeClases/Models/Clase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiagramaDeClases.Models
+{
+    public class Clase
+    {
+        protected List<Alumno> alumnos = new List<Alumno>();
+        protected Profesor profe;
+
+        public Clase(List<Alumno> alumnos, Profesor profe)
+        {
+            this.alumnos = alumnos;
+            this.profe = profe;
+        }
+
+        public float Media(){
+
+            return 2;
+        }
+
+        public void evaluarTodos(){
+            
+        }
+
+    }
+}
